47fcd7b7adef398734ba69bd5605b89d74b2926f
rcaporal <rafaelcaporal@gmail.com>
2017-07-10 06:57:57 +0000

    Correções feitas na página todo.html e adição de novos parâmetros de inicialização em BootStrap.groovy

--- grails-app/init/minhastarefas/BootStrap.groovy
+++ grails-app/init/minhastarefas/BootStrap.groovy
@@ -5,13 +5,15 @@ class BootStrap {
     def init = { servletContext ->
 
         def todos = [
-                [name: 'limpar a casa'],
-                [name: 'agua do cachorro'],
-                [name: 'alimentar o cachorro'],
-                [name: 'pagar contas'],
-                [name: 'correr'],
-                [name: 'nadar']
-        ].each { new Todo(name: it.name).save() }
+                [name: 'limpar a casa', completed: false],
+                [name: 'agua do cachorro', completed: false],
+                [name: 'alimentar o cachorro', completed: false],
+                [name: 'pagar contas', completed: false],
+                [name: 'correr', completed: true],
+                [name: 'nadar', completed: true]
+        ].each { new Todo(name: it.name, completed: it.completed).save()
+
+        }
 
     }
     def destroy = {
